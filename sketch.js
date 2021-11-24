15d49d07aefe34a975b75875e3a766b2c68ec190
Kyle Stewart <kylestew@gmail.com>
2021-11-24 15:47:20 -0700

    Anti-aliasing in a cheap way

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 import { circle, asPolygon } from "@thi.ng/geom";
-import { sub } from "@thi.ng/vectors";
+import * as v from "@thi.ng/vectors";
 import * as sdf from "./lib/sdf_2d";
 import { rayMarch } from "./lib/ray_march";
 import { Ray } from "./lib/ray";
@@ -9,20 +9,26 @@ const settings = {
   // animated: true,
   clearColor: "black",
 
-  rayCount: 8,
+  rayCount: 1000,
 
-  rayMarchMaxSteps: 12,
-  rayMarchMaxDist: 4.0,
-  rayMarchSurfDist: 0.0001,
+  antiAliasFactor: 0.001,
+
+  rayMarchMaxSteps: 32,
+  rayMarchMaxDist: 3.0,
+  rayMarchSurfDist: 0.001,
 };
 
 // 1) Create one or more shapes
-const shapes = [sdf.circle([0, 0], 0.3)];
+const shapes = [
+  sdf.circle([-0.5, 0], 0.3),
+  sdf.circle([0.5, 0], 0.3),
+  sdf.circle([0, 0], 0.05),
+];
 
 // 2) Create N points of light
 const lights = [
-  { pt: [-0.4, -0.6], cd: [1, 0, 0, 1] },
-  { pt: [0.6, 0.4], cd: [1, 0, 0, 1] },
+  { pt: [-0.3, -0.6], cd: [0, 0.7, 0.8, 0.7] },
+  { pt: [0.3, 0.6], cd: [0, 0.8, 0.7, 0.7] },
 ];
 
 // 3) Fire rays from point lights in all directions
@@ -35,13 +41,23 @@ function fireRays(light) {
   let poly = asPolygon(circle(light.pt, 0.1), settings.rayCount);
   let rays = poly.points.map((pt) => {
     // direction = point on circle - center of circle
-    return Ray(light.pt, sub([], pt, light.pt));
+
+    let cent = v.add([], light.pt, [
+      settings.antiAliasFactor * (Math.random() - 0.5),
+      settings.antiAliasFactor * (Math.random() - 0.5),
+    ]);
+
+    return Ray(cent, v.sub([], pt, light.pt));
   });
 
   // fire from each ray and collect lines made
   return rays.map((ray) => {
     let rec = rayMarch(ray, shapes, settings);
-    return ray.lineTo(rec.dist);
+    let line = ray.lineTo(rec.dist);
+    line.attribs = {
+      cd: light.cd,
+    };
+    return line;
   });
 }
 
@@ -53,15 +69,27 @@ const update = function () {
 
 function render({ ctx, canvasScale }) {
   // ctx.strokeStyle = "#ffffff11";
+  ctx.fillStyle = "#ffffff22";
   ctx.strokeStyle = "#ffffff";
 
-  shapes.forEach((shape) => {
-    dx.circle(ctx, shape.pos, shape.r);
-  });
+  // shapes.forEach((shape) => {
+  //   dx.circle(ctx, shape.pos, shape.r);
+  // });
 
   lines.forEach((line) => {
-    console.log(line);
     let [a, b] = line.points;
+
+    if (line.attribs.cd) {
+      let cd = line.attribs.cd.map((a) => {
+        return a * 255;
+      });
+      ctx.strokeStyle = `rgba(
+      ${cd[0]},
+      ${cd[1]},
+      ${cd[2]},
+      ${line.attribs.cd[3]})`;
+    }
+
     dx.line(ctx, a, b);
   });
 }
