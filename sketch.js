3f298c84d8dbd01aa0b2decb00d0989c444bc9b6
Kyle Stewart <kylestew@gmail.com>
2021-11-26 10:40:25 -0700

    Rays bounce off surfaces, wow

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
-import { circle, asPolygon } from "@thi.ng/geom";
+import { circle, asPolygon, transform } from "@thi.ng/geom";
 import * as v from "@thi.ng/vectors";
+import * as m from "@thi.ng/matrices";
 import * as sdf from "./lib/sdf_2d";
 import { rayMarch } from "./lib/ray_march";
 import { Ray } from "./lib/ray";
 import * as dx from "./snod/drawer";
+import { linspace } from "./snod/math";
+import { bounce } from "cli-spinners";
 
 const settings = {
   // animated: true,
   clearColor: "black",
 
-  rayCount: 1000,
+  rayCount: 256,
 
-  antiAliasFactor: 0.001,
+  antiAliasFactor: 0.0,
 
   rayMarchMaxSteps: 32,
   rayMarchMaxDist: 3.0,
@@ -20,28 +23,35 @@ const settings = {
 
 // 1) Create one or more shapes
 const shapes = [
-  sdf.circle([-0.5, 0], 0.3),
-  sdf.circle([0.5, 0], 0.3),
-  sdf.circle([0, 0], 0.05),
+  sdf.circle([0, 0], 0.2),
+  // sdf.circle([-0.5, 0], 0.3),
+  // sdf.circle([0.5, 0], 0.3),
+  // sdf.circle([0, 0], 0.05),
 ];
 
 // 2) Create N points of light
 const lights = [
-  { pt: [-0.3, -0.6], cd: [0, 0.7, 0.8, 0.7] },
-  { pt: [0.3, 0.6], cd: [0, 0.8, 0.7, 0.7] },
+  { pt: [0.5, 0.5], cd: [0, 0.7, 0.8, 0.7] },
+  // { pt: [-0.3, -0.6], cd: [0, 0.7, 0.8, 0.7] },
+  // { pt: [0.3, 0.6], cd: [0, 0.8, 0.7, 0.7] },
 ];
 
 // 3) Fire rays from point lights in all directions
 // a) draw rays in color of light
-// TODO: b) bounce rays that collide with CIRCLE and pick up their color
+// b) bounce rays that collide with CIRCLE and pick up their color
 function fireRays(light) {
-  // point lights fire rays in all directions
-  // create a circle and use points as directions
-  // (saves on some brain-thinking)
-  let poly = asPolygon(circle(light.pt, 0.1), settings.rayCount);
-  let rays = poly.points.map((pt) => {
-    // direction = point on circle - center of circle
+  // create a set of unit points equally in all directions around center point
+  let thetaOffset = Math.PI / 4.0 - 0.1;
+  let points = linspace(0, 2 * Math.PI, settings.rayCount).map((theta) => {
+    return [
+      light.pt[0] + Math.cos(theta + thetaOffset),
+      light.pt[1] + Math.sin(theta + thetaOffset),
+    ];
+  });
 
+  // create rays in the direction of those points
+  let rays = points.map((pt) => {
+    // direction = point on circle - center of circle
     let cent = v.add([], light.pt, [
       settings.antiAliasFactor * (Math.random() - 0.5),
       settings.antiAliasFactor * (Math.random() - 0.5),
@@ -50,14 +60,29 @@ function fireRays(light) {
     return Ray(cent, v.sub([], pt, light.pt));
   });
 
-  // fire from each ray and collect lines made
-  return rays.map((ray) => {
+  // fire each ray and collect lines made
+  return rays.flatMap((ray) => {
     let rec = rayMarch(ray, shapes, settings);
+
     let line = ray.lineTo(rec.dist);
     line.attribs = {
       cd: light.cd,
     };
-    return line;
+
+    let lines = [line];
+
+    // TODO: HACK: display bounce
+    if (rec.hit === true) {
+      let { p, n } = rec;
+      let newDirection = Ray(p, n);
+      let bounceLine = newDirection.lineTo(0.24);
+      bounceLine.attribs = {
+        cd: [1, 0, 0, 1],
+      };
+      lines.push(bounceLine);
+    }
+
+    return lines;
   });
 }
 
@@ -72,9 +97,9 @@ function render({ ctx, canvasScale }) {
   ctx.fillStyle = "#ffffff22";
   ctx.strokeStyle = "#ffffff";
 
-  // shapes.forEach((shape) => {
-  //   dx.circle(ctx, shape.pos, shape.r);
-  // });
+  shapes.forEach((shape) => {
+    dx.circle(ctx, shape.pos, shape.r);
+  });
 
   lines.forEach((line) => {
     let [a, b] = line.points;
